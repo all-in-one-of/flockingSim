f98c1145052a58635ab0c9801bdc3befb7b06f38
kateedge100 <kateedge@hotmail.com>
2018-04-14 15:08:09 +0100

    still trying 2

---
+++ FlockingSimTest/main.cpp
@@ -0,0 +1,42 @@
+/****************************************************************************
+basic OpenGL demo modified from http://qt-project.org/doc/qt-5.0/qtgui/openglwindow.html
+****************************************************************************/
+
+#include <iostream>
+#include <time.h>
+#include <sys/time.h>
+
+
+#include "flock_cpu.h"
+#include "flockSim_gpu.h"
+
+
+
+int main(int argc, char **argv)
+{
+
+
+    // GPU Flocking
+    FlockGPU::FlockingSim();
+
+
+
+    // CPU Flocking
+    Flock_CPU flock1;
+
+    flock1.flock(argc, argv);
+
+
+
+
+
+
+
+    return EXIT_SUCCESS;
+
+
+
+}
+
+
+
